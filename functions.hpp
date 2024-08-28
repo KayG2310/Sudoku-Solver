701aa416678c0465f4747b8a8ee302517568086c
Kamakshi Gupta <142660743+KayG2310@users.noreply.github.com>
2024-08-28 23:44:17 +0530

    header file

---
+++ functions.hpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+bool valid(vector<vector<char>> &board, int r, int c, char entry);
+
+bool solve(vector<vector<char>> &board);
+
+void solveSudoku(vector<vector<char>>& board);
